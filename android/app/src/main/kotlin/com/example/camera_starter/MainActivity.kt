aa50ab86195d57f93fb5b738cc01a75ea43722dd
Anatoly <waveinspectro@gmail.com>
2025-12-17 16:42:46 +0100

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/camera_starter/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.camera_starter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val nativeDetector = NativeDetector()
+        val message = nativeDetector.stringFromJNI()
+
+        setContent {
+            Text(text = message)
+        }
+    }
+}
+    
